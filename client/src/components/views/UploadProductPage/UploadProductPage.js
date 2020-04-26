0a412144083b10711d1ea1d5a89297ecac406ee8
Jisoon <siriusda2@naver.com>
2020-04-27 02:40:55 +0900

    DBへProduct保存

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
 import FileUpload from '../../utils/FileUpload';
+import Axios from 'axios';
 
 const { Title } = Typography;
 const { TextArea } = Input;
@@ -15,7 +16,7 @@ const Continents = [
     {key: 7, value: "Antarctica"}
 ]
 
-function UploadProductPage() {
+function UploadProductPage(props) {
 
     const [TitleValue, setTitleValue] = useState("");
     const [DescriptionValue, setDescriptionValue] = useState("");
@@ -45,6 +46,30 @@ function UploadProductPage() {
         setImages(newImage);
     }
 
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        const variables = {
+            writer: props.user.userData._id,
+            title: TitleValue,
+            description: DescriptionValue,
+            price: PriceValue,
+            images: Images,
+            continents: ContinentValue
+
+        }
+
+        Axios.post('/api/product/uploadProduct', variables)
+            .then(response => {
+                if(response.data.success) {
+                    alert('Product Successfully Uploaded');
+                    props.history.push('/');
+                } else {
+                    alert('Failed to upload Product');
+                }
+            })
+    }
+
     return (
         <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
 
@@ -52,7 +77,7 @@ function UploadProductPage() {
                 <Title level={2}> Upload Travel Product</Title>
             </div>
 
-            <Form onSubmit >
+            <Form onSubmit={onSubmit} >
 
             {/* DropZone */}
                 <FileUpload refreshFunction={updateImages}/>
@@ -90,7 +115,7 @@ function UploadProductPage() {
                 <br />
 
                 <Button
-                    onClick
+                    onClick={onSubmit}
                 >
                     Submit
                 </Button>
--- server/routes/product.js
+++ server/routes/product.js
@@ -39,4 +39,14 @@ router.post("/uploadImage", auth, (req, res) => {
     })
 });
 
+router.post("/uploadProduct", auth, (req, res) => {
+
+    const product = new Product(req.body)
+
+    product.save((err) => {
+        if(err) return res.status(400).json({ success: false, err })
+        return res.status(200).json({ success: true })
+    })
+});
+
 module.exports = router;
