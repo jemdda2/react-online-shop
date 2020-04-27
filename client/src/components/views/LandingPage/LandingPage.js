d94fcffc474aef414de7755ae8c122b7ac8c0bf2
Jisoon <siriusda2@naver.com>
2020-04-28 00:40:41 +0900

    LandingPageとImageSlider作成

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,14 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
+import { Icon, Col, Card, Row } from 'antd';
+import Axios from 'axios';
+
+import ImageSlider from '../../utils/ImageSlider'
+
+const { Meta } = Card;
 
 function LandingPage() {
+
+    const [Products, setProducts] = useState([])
+
+    useEffect(() => {
+        Axios.post('/api/product/getProducts')
+            .then(response => {
+                if (response.data.success) {
+                    setProducts(response.data.products)
+                } else {
+                    alert('Failed to factch product datas')
+                }
+            })
+    }, [])
+
+    const renderCards = Products.map((product, index) => {
+
+        return <Col lg={6} md={8} xs={24}>
+            <Card
+                Hoverable={true}
+                cover={<ImageSlider images={product.images}/>}
+            >
+                <Meta
+                    title={product.title}
+                    description={`$${product.price}`}
+                />
+            </Card>
+        </Col>
+    })
+
     return (
-        <>
-        <div className="app">
-            <FaCode style={{ fontSize: '4rem' }} /><br />
-            <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div style={{ width: '75%', margin: '3rem auto' }}>
+            <div style={{ textAlign: 'center' }}>
+                <h2> Let's Travel Anywhere <Icon type="rocket" /> </h2>
+            </div>
+
+            {/* { Filter } */}
+
+            {/* { Search } */}
+            {Products.length === 0 ?
+                <div style={{ display: 'flex', height: '300px', justifyContent: 'center', alignItems: 'center' }}>
+                    <h2>No post yet...</h2>
+                </div>  :
+                <div>
+                    <Row gutter={[16, 16]}>
+                        {renderCards}
+                    </Row>
+                </div>
+                
+            }
+            <br /><br />
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <button>Load More</button>
+            </div>
         </div>
-        </>
     )
 }
 
--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -49,6 +49,11 @@ function UploadProductPage(props) {
     const onSubmit = (event) => {
         event.preventDefault();
 
+        if(!TitleValue || !DescriptionValue || !PriceValue ||
+            !ContinentValue || !Images) {
+                return alert('fill all the fields first!')
+            }
+
         const variables = {
             writer: props.user.userData._id,
             title: TitleValue,
